e9f64c1e2d27dc96cb184ffbbe941d7d073a5523
Aeron6969 <102769264@students.swinburne.edu.my>
2025-04-08 13:18:53 +0800

    lolol

--- src/Pages/Blogmenu.jsx
+++ src/Pages/Blogmenu.jsx
@@ -1,10 +1,18 @@
 import BackButton from '../components/BackButton';
+import Firstheader from '../components/Firstheader';
+import Footer1 from '../components/Footer1';
 import "../styles.css";
 
 export default function Blogmenu() {
   return (
-    <div>
-      <BackButton />
-    </div>
+    <>
+      <header>
+        <Firstheader></Firstheader>
+      </header>
+
+        
+
+      <Footer1></Footer1>
+    </>
   );
 }
