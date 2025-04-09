1aee2bc37551cf757d86b89ff280851bd6fd016b
dlim4469 <102779043@students.swinburne.edu.my>
2025-04-09 08:28:56 +0800

    Blogs failure

---
+++ src/Pages/blogs.jsx
@@ -0,0 +1,82 @@
+import Firstheader from "../components/Firstheader";
+import Footer1 from "../components/Footer1";
+import "../styles.css";
+import blogs1 from "../assets/smg1.png";
+
+export default function Blogs() {
+  return (
+    <>
+      <header>
+        <Firstheader />
+      </header>
+
+      
+  <div classname="smg1blogs">
+    <img src="{blogs1}" alt="TerraGuide Logo" />
+  </div>
+  <div classname="smg1blogsh1">
+    <h1>The History of Semengguh</h1>
+  </div>
+  <div classname="smg1blogsp1">
+    Semenggoh Wildlife Centre enables tourists to interact with wild orang utans
+    and enjoy the orang utans feeding time for the amazing experience. For more
+    than thirty years, Semenggoh’s staffs have trained young orang utans how to
+    survive in the wild. The programme has successfully provided those orang
+    utans, which had been orphaned or rescued from captivity, to have their
+    freedom by living in a natural habitat.
+  </div>
+  <div classname="smg1blogsp2">
+    The tour operators usually arrange trips to Semenggoh Wildlife Centre
+    combining with other local attractions. With the potential attraction and
+    marketability of Semenggoh Wildlife Centre, it is vitally important to look
+    into the tourists’ perceptions towards destination competitiveness. To date,
+    destination competitiveness research has been matured enough to clearly
+    define the competitiveness and data gathering is likely to be more feasible
+    and reliable. This is evident from the research by Liu (2006), who asserts
+    that the Malaysian context in rural tourism is mostly related to the
+    determined ethnic disparities, geographically, socio-culturally and
+    economically.
+  </div>
+  <div classname="smg1blogsp3">
+    Jenkins (1999) conducted a research on the conception of destination images
+    as perceived by tourists and those reflected in different academic subjects
+    and also by practitioners. According to Lawson and Bovy (1977), definition
+    for destination image refers to the expression of all imaginations,
+    objective knowledge and emotional thoughts of a group of people or even an
+    individual. To improve and sustain destination competitiveness, it is vital
+    to acknowledge that higher tourist satisfaction level should be established
+    by creating a positive post-purchase tourist behaviour Yoon &amp; Uysal,
+    2003. In such case, Alegre and Garau 2010 conducted a survey to examine the
+    tourists’ overall satisfaction and dissatisfaction as well as their
+    intention to return to the specific destination. Nature resources refer to
+    the naturally occurring sources of wealth for a specific region which may
+    consist of land, forests, mineral deposits and water that occur in nature
+    and can be used for economic gain. Nature resources are important for human
+    to survive and contribute to the economy of the nation in which they exist.
+    Mazilu (2012) defined destination as a complex element connecting
+    geographical resources or the natural and anthropic dimensions that are
+    accessible by tourists. Dugulan, Balaure, Popescu and Veghes (2010) strongly
+    agree that natural resources and cultural heritage are two important facets
+    in tourism’s sustainable development. Such contribution is crucial to the
+    whole competitiveness of the tourism industry and its performances by
+    qualitative point of view. As cited in Dugulan, et.al. (2010), Lumsdon
+    (1997) proposes that natural resource is one of the main pillars to support
+    tourist destination competitiveness.
+  </div>
+
+
+
+<Footer1 />
+</>
+);
+}
+
+
+
+
+
+
+
+
+
+
